a3b1c206182145548fafed740ef37699e7f7c4cc
Quant2Trade <quant2trade@gmail.com>
2019-08-06 04:19:14 -0300

    _

--- app/db_lib/connection.py
+++ app/db_lib/connection.py
@@ -1,13 +1,9 @@
 import mysql.connector
 import pandas as pd
 
-# HOST = 'localhost'
-# USER = 'root'
-# PWD = 'F1nt5yn6!'
-
-HOST = '3.217.217.48'
-USER = 'romi'
-PWD = 'romiconnect'
+HOST = 'localhost'
+USER = 'root'
+PWD = 'F1nt5yn6!'
 
 
 def _init_connection(host=HOST, user=USER, password=PWD):
