4479abec76818ee7cec2ad60dadf46c5873ed5d3
Quant2Trade <quant2trade@gmail.com>
2019-09-25 04:58:47 -0300

    _

--- app/views/request_mes_data.py
+++ app/views/request_mes_data.py
@@ -47,5 +47,7 @@ def download_mes(machine_id='1'):
     path = app.root_path + '/static/res/out/'
     mes.to_csv(path + filename, index=False)
     start = None; end = None; mid = None
+    out = send_file(path + filename, as_attachment=True, attachment_filename=filename, cache_timeout=1)
+    os.remove(path+filename)
 
-    return send_file(path + filename, as_attachment=True, attachment_filename=filename, cache_timeout=1)
+    return out
